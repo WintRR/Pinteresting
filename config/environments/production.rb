d055196ac9d6b9bb21b04a41ec682d9227c7a244
Andy B <Andydkbui@gmail.com>
2014-06-04 12:05:58 +1000

    Add amazon s3 for paperclip upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,16 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  #S Sets Paperclip to upload images to S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
